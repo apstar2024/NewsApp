8cb6bc6fcf004c427c97e6c9719712902fb4b998
apstar2024 <115718097+apstar2024@users.noreply.github.com>
2023-01-22 15:49:32 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -1,23 +1,37 @@
 import React, { Component} from "react";
 import Loading from "./Loading";
 import Newsitem from "./newsItem";
+import PropTypes from 'prop-types'
 
-export default class News extends Component { 
-  constructor() {
-    super();
+
+export default class News extends Component {  
+  static defaultProps = {
+    category: 'General',
+    pageSize:8,
+    country:'in',
+    title:'Star News'
+  }
+  static propTypes = {
+    category: PropTypes.string,
+    pageSize:PropTypes.number,
+    country:PropTypes.string
+  };
+  constructor(props) {
+    super(props);
     // console.log("this is constructor from news component");
     this.state = {
       articles: [],
       loading: true,
       pageno:1,
-      totalpage:2
+      totalpage:2,
     };
+    document.title=`${this.props.category}- News`;
   }
   goBack=async()=>{
     console.log('going back');
     this.setState({loading:true})
     this.setState({pageno:this.state.pageno-1});
-    let urlapi=`https://newsapi.org/v2/top-headlines?country=in&pageSize=${this.props.pageSize}&page=${this.state.pageno}&apiKey=ab58b0a06436481dbac3e440f44ef8af`;
+    let urlapi=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&pageSize=${this.props.pageSize}&page=${this.state.pageno}&apiKey=ab58b0a06436481dbac3e440f44ef8af`;
     let data=await fetch(urlapi);
     let usabledata=await data.json();
     this.setState({articles:usabledata.articles,loading:false})
@@ -26,14 +40,14 @@ export default class News extends Component {
     console.log('going next');
     this.setState({loading:true})
     this.setState({pageno:this.state.pageno+1});
-    let urlapi=`https://newsapi.org/v2/top-headlines?country=in&pageSize=${this.props.pageSize}&page=${this.state.pageno}&apiKey=ab58b0a06436481dbac3e440f44ef8af`;
+    let urlapi=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&pageSize=${this.props.pageSize}&page=${this.state.pageno}&apiKey=ab58b0a06436481dbac3e440f44ef8af`;
     let data=await fetch(urlapi);
     let usabledata=await data.json();
     this.setState({articles:usabledata.articles,loading:false})
   }
   async componentDidMount(){
     this.setState({loading:true})
-    let urlapi=`https://newsapi.org/v2/top-headlines?country=in&pageSize=${this.props.pageSize}&page=${this.state.pageno}&apiKey=ab58b0a06436481dbac3e440f44ef8af`;
+    let urlapi=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&pageSize=${this.props.pageSize}&page=${this.state.pageno}&apiKey=ab58b0a06436481dbac3e440f44ef8af`;
     let data=await fetch(urlapi);
     let usabledata=await data.json();
     this.setState({articles:usabledata.articles,loading:false})
@@ -45,7 +59,7 @@ export default class News extends Component {
       {this.state.loading &&<Loading/>}
       <div className="container">
         <div className="row row-cols-1 row-cols-md-4 g-3">
-          {this.state.loading==false && this.state.articles.map((element) => {
+          {this.state.loading===false && this.state.articles.map((element) => {
             return (
               <div className="col" key={element.url}>
                 <Newsitem
@@ -53,15 +67,18 @@ export default class News extends Component {
                   descrpition={element.content?element.content.slice(0,80):""}
                   imgurl={element.urlToImage?element.urlToImage:"./unavailable.jpg"}
                   newsUrl={element.url}
+                  author={element.author}
+                  publishedAt={element.author}
+                  source={element.source.name}
                 />
               </div>
             );
           })}
         </div>
-            <div className="container d-flex justify-content-between my-2">
+            {this.state.loading===false && <div className="container d-flex justify-content-between my-2">
               <button type="button" disabled={this.state.pageno===1} className="btn btn-primary" onClick={this.goBack}>Previous</button>
-              <button type="button" disabled={this.state.totalpage==this.state.pageno} className="btn btn-primary" onClick={this.goNext}>Next</button>
-          </div>
+              <button type="button" disabled={this.state.totalpage===this.state.pageno} className="btn btn-primary" onClick={this.goNext}>Next</button>
+           </div>}
       </div>
       </>
     );
