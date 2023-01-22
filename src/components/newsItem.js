d32688fe9a6482168e8b4eeac48c5ffb9f60cd44
apstar2024 <115718097+apstar2024@users.noreply.github.com>
2023-01-22 15:52:00 +0530

    Update newsItem.js

--- src/components/newsItem.js
+++ src/components/newsItem.js
@@ -2,25 +2,34 @@ import React, { Component } from "react";
 
 export default class newsItem extends Component {
   render() {
-    const {Title,descrpition,imgurl,newsUrl}=this.props;
+    const { Title, descrpition, imgurl, newsUrl, author, publishedAt, source } =
+      this.props;
     return (
-      // <div className="card col" style={{ width: "18rem" }}>
-      //   <img src="..." className="card-img-top" alt="/" />
-      //   <div className="card-body">
-      //     <p className="card-text">
-      //       Some quick example text to build on the card title and make up the
-      //       bulk of the card's content.
-      //     </p>
-      //   </div>
-      // </div>
       <div className="card">
+      {/* //   <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style={{zIndex:1,left:'82%!Important'}}>
+      //     {source}
+      //     <span class="visually-hidden">unread messages</span>
+      //   </span> */}
         <img src={imgurl} className="card-img-top" alt="/" />
         <div className="card-body">
-          <h5 className="card-title" style={{color:"blue",fontWeight:"bolder",fontSize:"13px"}}>{Title}..</h5>
-          <p className="card-text">
+          <h5
+            className="card-title"
+            style={{ color: "blue", fontWeight: "bolder", fontSize: "13px" }}
+          >
+            {Title}..
+          </h5>
+          <p className="card-text" style={{ marginBottom: "0.2rem" }}>
             {descrpition}...
           </p>
-          <a href={newsUrl} className="btn btn-sm btn-primary" target={'_blank'}>
+          <div style={{ fontSize: "9px", color: "darkgrey" }}>
+            By: {author ? author : "unknown"} on:
+            {new Date(publishedAt).toGMTString()}
+          </div>
+          <a
+            href={newsUrl}
+            className="btn btn-sm btn-primary"
+            target={"_blank"}
+          >
             ReadMore..
           </a>
         </div>
