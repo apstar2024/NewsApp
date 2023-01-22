73d5dc9f25e76bbb94974611e9c3d18a6e359f80
apstar2024 <115718097+apstar2024@users.noreply.github.com>
2023-01-22 15:55:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 // import logo from './logo.svg';
-import './App.css';
- import React, { Component } from 'react'
-import Navbar from './components/Navbar';
-import News from './components/News';
- 
- export default class App extends Component {
-   render() {
-     return (
-       <>
-       <Navbar/>
-       <News pageSize={6}/>
-       </>
-     )
-   }
- }
- 
+import "./App.css";
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  // Link
+} from "react-router-dom";
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <Router>
+          <Navbar />
+          <Routes>
+            {/* <News pageSize={8} category='Sports' /> */}
+            <Route exact path="/" element={<News pageSize={8} category="general" key={'General'}/>}/>
+            <Route exact path="/general" element={<News pageSize={8} category="general" key={'General'}/>}/>
+            <Route exact path="/Business" element={<News pageSize={8} category="Business" key={'Business'}/>}/>
+            <Route exact path="/Entertainment" element={<News pageSize={8} category="Entertainment" key={'Entertainment'}/>}/>
+            <Route exact path="/Health" element={<News pageSize={8} category="Health" key={'Health'}/>}/>
+            <Route exact path="/Science" element={<News pageSize={8} category="Science" key={'Science'}/>}/>
+            <Route exact path="/Sports" element={<News pageSize={8} category="Sports" key={'Sports'}/>}/>
+            <Route exact path="/Technology" element={<News pageSize={8} category="Technology" key={'Technology'}/>}/>
+          </Routes>
+        </Router>
+      </>
+    );
+  }
+}
